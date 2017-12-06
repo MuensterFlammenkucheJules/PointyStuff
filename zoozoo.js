0fc39240e7c1b1a0c661d8335492ae019c05116f
TheZT2RoundTable <33802761+TheZT2RoundTable@users.noreply.github.com>
2017-12-06 15:46:19 +0100

    Add files via upload

---
+++ zoozoo.js
@@ -0,0 +1,19 @@
+//<![CDATA[
+
+var t_award2 = {
+
+name : "Old Zoo",
+
+thumbnail : [20,20],
+
+closeFunction : "fade",
+
+users : [
+
+[3525512,"Bald Eagle","http://i.imgur.com/XgYiaju.png","Thank you for helping us start up the site!","March 3, 2013"],
+
+]
+
+}
+
+//]]>
