b33f257a51c17e3947f929dbf818d22122a16b59
TheZT2RoundTable <33802761+TheZT2RoundTable@users.noreply.github.com>
2017-12-06 21:22:07 +0100

    Add files via upload

---
+++ members.js
@@ -0,0 +1,451 @@
+/* created by Holysavior of ZNR*/
+
+var sym = "#"; // Prefix symbol
+
+var iname = "Pointies"; // Field name
+
+var app = 3; // Amount per post
+
+
+
+myusers = new Array(); // myusers[y]=["USER ID", AMOUNT];
+
+myusers[0] = ["3525929", -157]; // Michael
+
+myusers[1] = ["3854994", -80]; // Ursek
+
+myusers[2] = ["3990249", 736]; // Plushtrap
+
+myusers[3] = ["3616304", -50]; // benjasaurus1
+
+myusers[4] = ["3949024", -1265]; // Rebbie
+
+myusers[5] = ["3535507", -132]; // Alex
+
+myusers[6] = ["3525967", -1090]; // Scottslive21
+
+myusers[7] = ["3526525", 1451]; // Kevin
+
+myusers[8] = ["3566624", -95]; // Rudyn
+
+myusers[9] = ["3539659", -95]; // Mysty
+
+myusers[10] = ["3526476", -155]; // Jasper
+
+myusers[11] = ["3564315", -173]; // PrimevalRaptor
+
+myusers[12] = ["3526831", -173]; // Eryel
+
+myusers[13] = ["3530161", -580]; // Jesse
+
+myusers[14] = ["3532923", -191]; // Animalboy
+
+myusers[15] = ["3527830", -1356]; // Dinosaure99
+
+myusers[16] = ["3558138", -1273]; // Arrancarh
+
+myusers[17] = ["3940202", -380]; // Slothy Dude
+
+myusers[18] = ["3629977", -296]; // Little Dog Chris
+
+myusers[19] = ["3870019", -1021]; // Odin
+
+myusers[20] = ["3597647", -344]; // Jannick
+
+myusers[21] = ["3564329", -705]; // Dragranzer
+
+myusers[22] = ["3535915", -1365]; // ultamateterex2
+
+myusers[23] = ["3576975", -377]; // Feral Designs
+
+myusers[24] = ["3592233", -745]; // MateoRex
+
+myusers[25] = ["3526593", -422]; // samuel
+
+myusers[26] = ["3526102", 98]; // Tyler
+
+myusers[27] = ["3525937", 367]; // lgcfm
+
+myusers[28] = ["3571112", -886]; // Ganeagle
+myusers[29] = ["3728433", -1934]; // Jem
+myusers[30] = ["3821200", -527]; // Bigseal
+myusers[31] = ["3524064", -345]; // Nils
+myusers[32] = ["3812531", 200]; // dycki1231
+myusers[33] = ["3534856", -272]; // Yur
+myusers[34] = ["4000164", -1761]; // Slappio
+myusers[35] = ["4055998", -770]; // Pheonix the fox
+myusers[36] = ["3591991", -3000]; // Anas platyrhynchos
+myusers[37] = ["3830787", 1839]; // Rowan
+myusers[38] = ["3642277", -1550]; // Elephas Maximus
+myusers[39] = ["3689182", -490]; // Unicorn
+myusers[40] = ["3532687", -731]; // Austroraptor
+myusers[41] = ["3891530", -1080]; // Sleeping Koala
+myusers[42] = ["3745497", -2046]; // Rezcall
+myusers[43] = ["3527308", 68]; // Chase
+myusers[44] = ["3693467", -540]; // Dinobear
+myusers[45] = ["3564310", -950]; // Hammond
+myusers[46] = ["3794791", -887]; // Ulquiorra
+myusers[47] = ["3527641", -920]; // TradeMark
+myusers[48] = ["3927582", 715];  // Papageno
+myusers[49] = ["3818741", -1712]; // Iron Man
+myusers[50] = ["3532656", -2431]; // Acinonyx Jubatus
+myusers[51] = ["3592146", -2169]; // The Beeg One
+myusers[52] = ["3565766", -1842]; // Ztlabraptor211
+myusers[53] = ["3560845", -1064]; // Kid Buu
+myusers[54] = ["3526597", -137]; // Thom
+myusers[55] = ["3575809", -2267]; // Panthera Leo
+myusers[56] = ["3565962", -1214]; // Crocodile
+myusers[57] = ["3527445", -1490]; // MakiCatta
+myusers[58] = ["3525976", -1274]; // TEB
+myusers[59] = ["3550553", -1289]; // TheZooTycooner14
+myusers[60] = ["3526393", -2752]; // Vision
+myusers[61] = ["3953598", -1340]; // Luca9108
+myusers[62] = ["3566096", -2285]; // Animalraja 
+myusers[63] = ["3644481", -1397]; // MarxRaptor
+myusers[64] = ["3893149", -162]; // Envy
+myusers[65] = ["3533461", -1442]; // Iben
+myusers[66] = ["3524088", -1454]; // Rafael
+myusers[67] = ["3591287", -1371]; // pokeruto
+myusers[68] = ["3524063", -1630]; // caviar
+myusers[69] = ["3607539", -1664]; // Meerkatmatt2
+myusers[70] = ["3564648", -2285]; // Avenged
+myusers[71] = ["3621684", -1927]; // Alterbean7
+myusers[72] = ["3607529", -1760]; // Bill
+myusers[73] = ["3602238", -2290]; // Manakel
+myusers[74] = ["3614092", -1772]; // Omenoor
+myusers[75] = ["3618086", -2577]; // bayu
+myusers[76] = ["3879601", -2735]; // ZooZa
+myusers[77] = ["3756986", -2431]; // Mastodon28
+myusers[78] = ["3634409", -2630]; // Hippopotamus.
+myusers[79] = ["3551234", -2279]; // Nicolas
+myusers[80] = ["3609639", -2390]; // Snoopy
+myusers[81] = ["3612679", -2423]; // Farkle
+myusers[82] = ["3590847", -2429]; // DRAGON-unit911
+myusers[83] = ["3524081", -2522]; // Imp
+myusers[84] = ["3589410", -3513]; // Megaraptorking
+myusers[85] = ["3544661", -3601]; // Flish
+myusers[86] = ["3662828", -2882]; // Swimming Spaghetti Monster
+myusers[87] = ["3602013", -2885]; // Ultron
+myusers[88] = ["3536477", -2734]; // Arrancon
+myusers[89] = ["3542941", -4310]; // Oviraptor
+myusers[90] = ["3945906", -4985]; // Burroughs
+myusers[91] = ["3850948", -4955]; // Narukota / Buddy The Elf
+myusers[92] = ["3526140", -3041]; // Nomis
+myusers[93] = ["3614342", -3236]; // Laura
+myusers[94] = ["3832038", -3097]; // Zoohuahua
+myusers[95] = ["3745532", -5083]; // Priya
+myusers[96] = ["3590226", -3575]; // Dr. Hax
+myusers[97] = ["3603946", -3536]; // OwlParrot
+myusers[98] = ["3526483", -3569]; // philly
+myusers[99] = ["3822293", -4525]; // Diabloceratops
+myusers[100] = ["3551021", -3905]; // Myrtle Snow
+myusers[101] = ["3730804", -2931]; // Kaleb
+myusers[102] = ["3525920", -3695]; // Hudson
+myusers[103] = ["3526832", -1508]; // Umpaloompa
+myusers[104] = ["3530430", -4208]; // Okeanos
+myusers[105] = ["3699299", -3730]; // Knertje2208
+myusers[106] = ["3564378", -3463]; // Tyranachu
+myusers[107] = ["3559857", -4305]; // Dacentru
+myusers[108] = ["3465719", -4355]; // jragonizer
+myusers[109] = ["3528535", -4154]; // davidy12
+myusers[110] = ["3655544", -5556]; // the dark phoenix
+myusers[111] = ["3691243", -4295]; // Hamikins
+myusers[112] = ["3682881", -4431]; // izzibob
+myusers[113] = ["3630490", -4622]; // HENDRIX
+myusers[114] = ["3592234", -5870]; // Gabriel12
+myusers[115] = ["3526032", -5287]; // extremos
+myusers[116] = ["3660427", -4811]; // Lazardi
+myusers[117] = ["3883327", -4478]; // Helios
+myusers[118] = ["3552565", -5168]; // Bigwhale
+myusers[119] = ["3526324", -5366]; // Orca Freak
+myusers[120] = ["3545461", -7386]; // Terrena Laxamamentum
+myusers[121] = ["3526544", -5501]; // Uolym
+myusers[122] = ["3534069", -7281]; // TheYeti
+myusers[123] = ["3465724", -3423]; // Zebrasorus
+myusers[124] = ["3560826", -6321]; // fridooh
+myusers[125] = ["3465774", -8291]; // Danny
+myusers[126] = ["3581977", -6373]; // hananas59
+myusers[127] = ["3540947", -7416]; // MiBound
+myusers[128] = ["3646520", -9608]; // Admiral General Aladeen
+myusers[129] = ["3833246", -14126]; // Humuhumu / Stitch
+myusers[130] = ["3563352", -7755]; // Murdock129
+myusers[131] = ["3526435", -7866]; // Franco
+myusers[132] = ["3638429", -5877]; // Burns
+myusers[133] = ["3524252", -7535]; // CRG
+myusers[134] = ["3527293", -8345]; // Stephen
+myusers[135] = ["3527471", -11968]; // Fluffs
+myusers[136] = ["3543875", -9266]; // Fireplume
+myusers[137] = ["3564354", -8681]; // Mathius Tyra
+myusers[138] = ["3533869", -10514]; // MrGorsh
+myusers[139] = ["3565764", -10772]; // stargatedalek
+myusers[140] = ["3649361", -12504]; // Flower
+myusers[141] = ["3526617", -14875]; // Sylvan
+myusers[142] = ["3715937", -15945]; // Robbie
+myusers[143] = ["3525512", -16094]; // Jules
+myusers[144] = ["3636620", -16697]; // CyborgIguana
+myusers[145] = ["3537401", -19455]; // Azrael
+myusers[146] = ["3524828", -20834]; // Cat
+myusers[147] = ["3525948", -32506]; // Furka
+myusers[148] = ["3526644", -38629]; // Ignacio
+myusers[149] = ["3525923", -2070]; // Komodo
+myusers[150] = ["3528099", -200]; // Bent
+myusers[151] = ["3529633", -290]; // 16529950
+myusers[152] = ["3547007", -130]; // Velez
+myusers[153] = ["3525015", -111]; // The Dragon Whisperer
+myusers[154] = ["3553036", -150]; // PrimevalDK
+myusers[155] = ["3535814", -600]; // Smaug/Athena
+myusers[156] = ["3564384", -1678]; // Frozen Orca
+myusers[157] = ["3526124", -311]; // dp197
+myusers[158] = ["3527447", -50]; // Ravenna
+myusers[159] = ["3585547", -350]; // BlitzHabu
+myusers[160] = ["3589814", -360]; // Teutonic
+myusers[161] = ["3528395", -350]; // Piece of Heaven
+myusers[162] = ["3556482", -40]; // Oxybelis
+myusers[163] = ["3527796", -25]; // Jorrit
+myusers[164] = ["3564401", -325]; // danl
+myusers[165] = ["3682451", -380]; // Royboy407
+myusers[166] = ["3564299", -40]; // The J.ster
+myusers[167] = ["3526281", -300]; // paul78giszewski
+myusers[168] = ["3601100", -700]; // Andrew
+myusers[169] = ["3740086", -1100]; // Helryx
+myusers[170] = ["3632125", -300]; // Nessich
+myusers[171] = ["3782178", -50]; // ZBD
+myusers[172] = ["3524135", -350]; // Jenna
+myusers[173] = ["3526670", -420]; // tigris115
+myusers[174] = ["3692744", -500]; // JPOG7
+myusers[175] = ["3775794", -283]; // velociraptor
+myusers[176] = ["3598991", -720]; // BossAggron
+myusers[177] = ["3593518", 395]; // Wormwood
+myusers[178] = ["3857102", -413]; // Firefoxrock
+myusers[179] = ["3876622", -440]; // Black Hawk
+myusers[180] = ["3606513", -100]; // Walrustooth
+myusers[181] = ["3529015", -500]; // Okapi Rainforest
+myusers[182] = ["3911826", -386]; // Shiro
+myusers[183] = ["3556536", -2150]; // Paleodude
+myusers[184] = ["3879793", -770]; // TheLastPanthera
+myusers[185] = ["3526556", -400]; // Gorgosaurus
+myusers[186] = ["3983193", -60]; // Catisdel
+myusers[187] = ["3930804", -850]; // ArianaGrande
+myusers[188] = ["3992362", -90]; // SGSolidGamer
+myusers[189] = ["4045313", 265]; // Mrpanch
+myusers[190] = ["4047746", -1529]; // Sergio
+myusers[191] = ["3958058", -6129]; // Zoo Tycooner FR
+myusers[192] = ["3526096", 508]; // Bunyupy
+myusers[193] = ["3526669", -1061]; // Man in Black
+myusers[194] = ["3525974", 595]; // MasterChief123
+myusers[195] = ["3526592", 550]; // JimmyzHoopz
+myusers[196] = ["3525960", 70]; // Kasatka-L
+myusers[197] = ["3526583", 721]; // Titan
+myusers[198] = ["3525934", 50]; // zooguy59
+myusers[199] = ["3526616", 50]; // Lord Vader
+myusers[200] = ["3526464", 31]; // Thomas
+myusers[201] = ["3525983", 550]; // Cosmius
+myusers[202] = ["3524232", 28]; // Twirl-a-Whirl
+myusers[203] = ["3526628", 598]; // Niels
+myusers[204] = ["3537914", 100]; // oO-VND-Oo
+myusers[205] = ["3541768", 352]; // iguanoraptor123
+myusers[206] = ["3526524", 200]; // Jordan
+myusers[207] = ["3526121", 120]; // Hondolover19
+myusers[208] = ["3529990", 50]; // bagas
+myusers[209] = ["3544214", 451]; // Platypus
+myusers[210] = ["3526870", 350]; // iWouldntMind
+myusers[211] = ["3554876", 328]; // Justice Society of America
+myusers[212] = ["3563970", 50]; // Eric
+myusers[213] = ["3547103", 835]; // monoslocos
+myusers[214] = ["3534732", 100]; // Nick23
+myusers[215] = ["3587177", 250]; // Kiwi
+myusers[216] = ["3584154", 85]; // zerosvalmont
+myusers[217] = ["3578317", 50]; // CtS
+myusers[218] = ["3580595", 50]; // Neofelis
+myusers[219] = ["3532407", 50]; // Clemsy
+myusers[220] = ["3582466", 340]; // Grumkrieg
+myusers[221] = ["3526612", 500]; // ewan27
+myusers[222] = ["3591302", 100]; // Collin
+myusers[223] = ["3538495", 200]; // Twinkle And Kessie
+myusers[224] = ["3593447", 718]; // Firefanix
+myusers[225] = ["3591110", 1073]; // Spinosail
+myusers[226] = ["3586639", 500]; // Penhuin
+myusers[227] = ["3591777", 100]; // Zephyr
+myusers[228] = ["3535866", 832]; // yodaboy
+myusers[229] = ["3630553", 375]; // French Marmot
+myusers[230] = ["3627826", 100]; // BenZooMaster
+myusers[231] = ["3629164", 200]; // Harlequinz eg0
+myusers[232] = ["3623046", 811]; // OwlEye
+myusers[233] = ["3524924", 300]; // Adam
+myusers[234] = ["3627201", 790]; // ZEVES
+myusers[235] = ["3591185", 565]; // orcadolphin
+myusers[236] = ["3627588", 55]; // Verdant Gregor
+myusers[237] = ["3590375", 300]; // TheGulo
+myusers[238] = ["3595072", 800]; // tigger
+myusers[239] = ["3608363", 350]; // Dino34909
+myusers[240] = ["3534848", 300]; // Pjurde
+myusers[241] = ["3618443", 337]; // Fisherman9192
+myusers[242] = ["3618824", 20]; // bob
+myusers[243] = ["3526276", 370]; // andersxhd
+myusers[244] = ["3638332", 800]; // crazy117
+myusers[245] = ["3557461", 300]; // AMRiver
+myusers[246] = ["3574164", 745]; // mstickmanp
+myusers[247] = ["3663790", 300]; // Saphire
+myusers[248] = ["3547108", 457]; // Pacific Time
+myusers[249] = ["3525963", 800]; // jazzymike
+myusers[250] = ["3666999", 500]; // Wolfhunter91
+myusers[251] = ["3647144", 316]; // l993
+myusers[252] = ["3643149", 105]; // The Zoo Builder
+myusers[253] = ["3637157", 500]; // dalhuisen12
+myusers[254] = ["3640279", 256]; // keenakeen
+myusers[255] = ["3607903", 100]; // Wendigo987
+myusers[256] = ["3554641", 220]; // cryptidman
+myusers[257] = ["3525954", 692]; // Maximilian
+myusers[258] = ["3675447", 22]; // Sahara
+myusers[259] = ["3706943", 200]; // joherca
+myusers[260] = ["3705204", 550]; // Spyra
+myusers[261] = ["3664676", 700]; // davidin
+myusers[262] = ["3657642", 808]; // Super Utah Thief
+myusers[263] = ["3692744", 253]; // doNDon0626
+myusers[264] = ["3720940", 300]; // Gerard
+myusers[265] = ["3601243", 740]; // mikek21
+myusers[266] = ["3679229", 300]; // aquila
+myusers[267] = ["3751049", 595]; // King Casque, Leader of Hornbills
+myusers[268] = ["3612721", 300]; // woodsy
+myusers[269] = ["3704028", 300]; // mistnight
+myusers[270] = ["3731259", 688]; // Australman
+myusers[271] = ["3696867", 400]; // ZooArtist
+myusers[272] = ["3600728", 700]; // stewe90
+myusers[273] = ["3542224", 950]; // Maks
+myusers[274] = ["3539138", 1167]; // Gamin' Wombat
+myusers[275] = ["3786866", 142]; // Emperor Sam
+myusers[276] = ["3750832", 500]; // AnimaDino
+myusers[277] = ["3536627", 500]; // lemo
+myusers[278] = ["3732378", 760]; // terrell
+myusers[279] = ["3693012", 500]; // MonsterZee
+myusers[280] = ["3627580", 100]; // quick brown fox
+myusers[281] = ["3793696", 820]; // gatorguy
+myusers[282] = ["3602522", -948]; // kepperbob
+myusers[283] = ["3794212", 100]; // Zancaslargas
+myusers[284] = ["3806373", 718]; // Bohemian Rhapsody
+myusers[285] = ["3577423", 800]; // zoology
+myusers[286] = ["3604313", 568]; // Scotty16
+myusers[287] = ["3813924", 115]; // Tunes
+myusers[288] = ["3826739", 400]; // Cwillaz
+myusers[289] = ["3803387", 480]; // exclamation mark
+myusers[290] = ["3713328", 1213]; // carnotaurus
+myusers[291] = ["3585308", 450]; // rappy
+myusers[292] = ["3822944", 300]; // paradisea
+myusers[293] = ["3839808", 100]; // Speedey
+myusers[294] = ["3824595", 7]; // Fredbear
+myusers[295] = ["3534176", 220]; // moki03
+myusers[296] = ["3780542", 200]; // Black Stallion
+myusers[297] = ["3780412", 200]; // ElephantBoy
+myusers[298] = ["3648364", 500]; // Pompaniddo
+myusers[299] = ["3786029", 304]; // arafan
+myusers[300] = ["3718839", 100]; // MonsterHunterGarth
+myusers[301] = ["3870197", 1225]; // Casey
+myusers[302] = ["3563171", 88]; // Gerben
+myusers[303] = ["3966944", 300]; // hi fish
+myusers[304] = ["3904491", 403]; // spendabuck
+myusers[305] = ["3817174", -170]; // Xenephos
+myusers[306] = ["3544756", 649]; // Auronoi-Cheiftain
+myusers[307] = ["3976967", 890]; // NeoLotus
+myusers[308] = ["3939509", 283]; // Lynxlynx
+myusers[309] = ["3934376", 307]; // concavenator
+myusers[310] = ["3689535", 634]; // tomiibex
+myusers[311] = ["3708742", 437]; // nige1
+myusers[312] = ["3854397", 600]; // Oceanana
+myusers[313] = ["3984893", 200]; // Louie Misciouscia
+myusers[314] = ["3933411", 600]; // Bongo
+myusers[315] = ["3832343", 300]; // tkgamer
+myusers[316] = ["3526222", 300]; // SLGray
+myusers[317] = ["3907997", 1613]; // Zhadow
+myusers[318] = ["3939492", 300]; // Carnoraptorsaur
+myusers[319] = ["3698932", 970]; // osteoprod
+myusers[320] = ["3804945", 300]; // -Nameless-
+myusers[321] = ["3813653", 300]; // tazra
+myusers[322] = ["3674810", 800]; // oryx13
+myusers[323] = ["3990638", 300]; // JK1011
+myusers[324] = ["3639216", 300]; // She-Wolf
+myusers[325] = ["3697287", 300]; // animalmaster12
+myusers[326] = ["3994831", 50]; // ThePsychoBear
+myusers[327] = ["3813409", 300]; // Hephaestus
+myusers[328] = ["3861460", 300]; // nightwars24
+myusers[329] = ["3978552", 300]; // Chips237
+myusers[330] = ["3998098", 600]; // Khaydar
+myusers[331] = ["4001636", 350]; // ShadowBeastReaper
+myusers[332] = ["3736736", 300]; // Flame
+myusers[333] = ["4006102", 31]; // Yutyrannus the second
+myusers[334] = ["3894166", -1198]; // Takara
+myusers[335] = ["4015706", 100]; // abs
+myusers[336] = ["3912590", -400]; // kbcoon
+myusers[337] = ["3987339", 500]; // Wombat22
+myusers[338] = ["3961587", 500]; // WolfyKurai
+myusers[339] = ["3875791", 500]; // LC Creations
+myusers[340] = ["3885729", 500]; // ZT2FF
+myusers[341] = ["3536919", 500]; // King_of_Animals
+myusers[342] = ["3943993", 500]; // nemo212
+myusers[343] = ["4003737", 500]; // snoopcoop
+myusers[344] = ["4005839", 500]; // Rush2112
+myusers[345] = ["4015515", 500]; // Rathlos
+myusers[346] = ["3673031", 230]; // Lelka
+myusers[347] = ["3962073", 352]; // babehunter1324
+myusers[348] = ["3665583", 120]; // Bebopgroove
+myusers[349] = ["3988992", 200]; // Wood Bison
+myusers[350] = ["3872416", 340]; // OceanaDesign
+myusers[351] = ["3882299", 150]; // Paleosaurus
+myusers[352] = ["4025708", 500]; // TyranntX
+myusers[353] = ["3816023", 750]; // Croc Master
+myusers[354] = ["4007598", 300]; // jagulep
+myusers[355] = ["4010655", -1213]; // Consultant
+myusers[356] = ["4008054", 1050]; // PaJaS30
+myusers[357] = ["4016350", 190]; // Chishio
+myusers[358] = ["3980240", 200]; // Galapagecko
+myusers[359] = ["3832574", 300]; // Dinovart
+myusers[360] = ["4037983", 50]; // godsaurus
+myusers[361] = ["3703875", 200]; // pantheradude
+myusers[362] = ["3613534", 400]; // lolzon
+myusers[363] = ["3767042", 100]; // Sherry
+myusers[364] = ["3803192", 200]; // bluebell12
+myusers[365] = ["4031981", -2025]; // Paleop
+myusers[366] = ["4032164", 10]; // Hugo7
+myusers[367] = ["4016350", 180]; // Chishio
+myusers[368] = ["4040414", -450]; // Gibbonnom
+myusers[369] = ["3958238", 2184]; // Rockypockypuff
+myusers[370] = ["4012517", 892]; // Scar
+myusers[371] = ["4078815", 1150]; // Serpyderpy
+myusers[372] = ["4024978", 250]; // Chickboy
+myusers[373] = ["3931867", 1000]; // Lord Celeborn
+myusers[374] = ["3943649", 250]; // JPFAN85
+myusers[375] = ["3838774", 250]; // julesf6
+myusers[376] = ["3928632", 155]; // Alex8730
+myusers[377] = ["4100584", -420]; // Hodorable
+myusers[378] = ["4123869", -220]; // Johto Boy
+myusers[379] = ["4117355", -1620]; // TheToastinator
+myusers[380] = ["4074122", 500]; // Urufu
+myusers[381] = ["3879931", 300]; // Gebo
+myusers[382] = ["4076138", -50]; // moy
+myusers[383] = ["4046979", -400]; // Batty
+myusers[384] = ["3988770", -925]; // Dylan
+myusers[385] = ["4155893", 100]; // RiasCreations
+myusers[386] = ["4159426", 1269674]; // Ebenezer Scrooge
+myusers[387] = ["4102796", 200]; // Sir_Pumpkinton
+myusers[388] = ["4105368", 100]; // Dino Keeper
+myusers[389] = ["4100737", 600]; // Amethyst
+myusers[390] = ["4156556", 750]; // bambam
+
+$(".user_info").each(function() {
+  var re = /,/g;
+  var icash = $(this).find("dt:contains('Posts:')").next().text();
+  icash = icash.replace(re, "");
+  $(this).append("<dt class='mid'>" + iname + ": </dt><dd class='thecount'>"+sym+"<span class='mcount'>" + parseInt(icash) * app + "</span></dd>")
+
+  theid = $(this).parent("td.c_user").parent().prev().children(".c_username ").children().attr("href");
+  userid = theid.split("/");
+
+  for (y = 0; y < myusers.length; y++) {
+    if (userid[4] == myusers[y][0]) {
+      var ichange = myusers[y][1];
+      var ame = $(this).find("dd.thecount").children().html();
+      var newcount = parseInt(ame) + ichange;
+      $(this).find("dd.thecount").children().html(newcount);
+    }
+
+  }
+});
