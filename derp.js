bf960b60e325e7c5a720be6c7d90d75b15b0e665
TheZT2RoundTable <33802761+TheZT2RoundTable@users.noreply.github.com>
2017-12-25 00:14:12 +0100

    Add files via upload

---
+++ derp.js
@@ -0,0 +1,21 @@
+//<![CDATA[
+
+var t_award2 = {
+
+name : "Old Zoo",
+
+thumbnail : [20,20],
+
+closeFunction : "fade",
+
+users : [
+
+[5160290,"Bald Eagle","http://i.imgur.com/XgYiaju.png","Thank you for helping us start up the site!","March 3, 2013"],
+
+[5160290,"Bald Eagle","http://i.imgur.com/XgYiaju.png","Thank you for helping us start up the site!","March 3, 2013"],
+
+]
+
+}
+
+//]]>
