14869f5e747db7b4536706dd81fe23e58aec2841
TheZT2RoundTable <33802761+TheZT2RoundTable@users.noreply.github.com>
2017-12-26 18:14:24 +0100

    Update derp.js

--- derp.js
+++ derp.js
@@ -10,7 +10,7 @@ closeFunction : "fade",
 
 users : [
 
-[5160290,"Bald Eagle","http://i.imgur.com/XgYiaju.png","Thank you for helping us start up the site!","March 3, 2013"],
+[5160290,"[i]Bald Eagle[/i]","http://i.imgur.com/XgYiaju.png","Thank you for "helping us" start up the site!","March 3, 2013"],
 
 [5160290,"Bald Eagle","http://i.imgur.com/XgYiaju.png","Thank you for helping us start up the site!","March 3, 2013"],
 
