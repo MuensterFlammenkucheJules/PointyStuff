2188adcc3425c55ccdfe6771cba94c3289349cbd
TheZT2RoundTable <33802761+TheZT2RoundTable@users.noreply.github.com>
2017-12-26 21:42:00 +0100

    Add files via upload

---
+++ fullnewzoo.js
@@ -0,0 +1,264 @@
+//<![CDATA[
+
+var t_award = {
+
+name : "Old Zoo",
+
+thumbnail : [20,20],
+
+closeFunction : "fade",
+
+users : [
+
+//cougarfan12
+[4187284,"Cougar","https://i.imgur.com/xVLt3vR.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Old_Dog
+[6150311,"Grey Wolf","https://i.imgur.com/63WpNhx.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Takara
+[3894166,"Orca","https://i.imgur.com/fljlqnd.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Yi Qi
+[3532687,"Siamang","https://i.imgur.com/y2F4dBu.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Guilmon
+[4004388,"Honey Badger","https://i.imgur.com/OvRt6wv.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//RGDS
+[6067973,"Tonkin Snub-Nosed Monkey","https://i.imgur.com/KyLKGnj.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Taigerus4
+[6138037,"Brown Basilisk","https://i.imgur.com/JC81oN6.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+[6138037,"Sea Wolf","https://i.imgur.com/szAXYwI.png","Third place at the Creature Creation V4 Contest","January 1, 2017"],
+
+//Cross
+[3824595,"Oarfish","https://i.imgur.com/OzJp4ds.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Arrancon
+[3536477,"Welsh Corgi Pembroke","https://i.imgur.com/o9kflkW.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//lgcfm
+[3525937,"Snow Leopard","https://i.imgur.com/FkdhocO.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Dylan
+[3988770,"Bali Starling","https://i.imgur.com/HqK8Pls.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//ZoologicalBotanist
+[6136261,"Ivory-Billed Woodpecker","https://i.imgur.com/VzTNOZZ.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Jules
+[3525512,"Hammerkop","https://i.imgur.com/lKyscLQ.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//TheYeti
+[3534069,"Blobfish","https://i.imgur.com/hwczYYH.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Cole12345
+[6017621,"Red River Hog","https://i.imgur.com/GZJvfor.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Ignacio
+[3526644,"Harpy Eagle","https://i.imgur.com/47qTtuJ.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Wes
+[3879601,"Common Kingfisher","https://i.imgur.com/Udyz91d.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//UraniumThylacine
+[4237659,"Siberian Tiger","https://i.imgur.com/zBXVKDk.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Scott
+[3525967,"Rainbow Agama","https://i.imgur.com/yL6svGb.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Imperator Furiosa
+[4132534,"African Forest Elephant","https://i.imgur.com/EcFfJI4.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Fireplume
+[3543875,"Ornate Hawk Eagle","https://i.imgur.com/YDJeWlC.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//caviar
+[3524063,"Andean Motmot","https://i.imgur.com/eQy1Fg3.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Orca Freak
+[3526324,"Orca","https://i.imgur.com/fljlqnd.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Danny
+[3465774,"Platypus","https://i.imgur.com/XaO93tB.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//antek234
+[6103681,"Spotted Genet","https://i.imgur.com/QMVV9GT.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Acinonyx Jubatus
+[3532656,"Anurognathus ammoni","https://i.imgur.com/GzBzJ8e.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+[3532656,"Wolfin","https://i.imgur.com/r09fcjm.png","First place at the Creature Creation V4 Contest","January 1, 2017"],
+
+//Okeanos
+[3530430,"Dacentrurus armatus","https://i.imgur.com/f3q1pqc.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Viggen
+[3728433,"Mantis Shrimp","https://i.imgur.com/0mc2jwv.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Burns
+[3638429,"Black-and-White Ruffed Lemur","https://i.imgur.com/Tz2Zbhh.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Azrael
+[3537401,"Gemsbok","https://i.imgur.com/3btsJe7.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//magpiealamode
+[4221754,"Lokotunjailurus emageritus","https://i.imgur.com/uHwd6Un.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Denomon3144	
+[3592146,"Thylacine","https://i.imgur.com/xrEAsW6.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//EsserWarrior
+[4079658,"Lar Gibbon","https://i.imgur.com/uO5QOia.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//xX LRD VDR Xx
+[6151935,"Hamadryas Baboon","https://i.imgur.com/eC2agIv.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Esbardo
+[6000826,"Striped Hyena","https://i.imgur.com/8K9z9U6.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Priya
+[3745532,"Asiatic Lion","https://i.imgur.com/pnj0Elc.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Dragon_Girl05
+[4222133,"Argali","https://i.imgur.com/IPTOPNu.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//pokeruto
+[3591287,"Axolotl","https://i.imgur.com/3TkHZfY.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Thom
+[3526597,"Platypus","https://i.imgur.com/XaO93tB.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Cheshire Litten
+[3990249,"Therizinosaurus cheloniformis","https://i.imgur.com/attuE5a.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Xenephos
+[3817174,"Linheraptor","https://i.imgur.com/zZxqRbf.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//HarryD28
+[6148673,"Carpet Python","https://i.imgur.com/aLFXFfu.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Stephen
+[3527293,"Border Collie","https://i.imgur.com/U3dO9Mf.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//AnimalGenius
+[4138608,"Coquerel's Sifaka","https://i.imgur.com/aBNe37V.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Bossman, Jake
+[4107732,"Blue-Tongued Skinki","https://i.imgur.com/cukXhba.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//TheToastinator
+[4117355,"Narwhal","https://i.imgur.com/kjPWX7m.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//SirHenrry
+[6097650,"Orinoco Crocodile","https://i.imgur.com/rgwzU5P.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Paleodude
+[3556536,"Majungasaurus crenatissimus","https://i.imgur.com/9tKSaZC.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//tigerdrake
+[6151109,"Indochinese Tiger","https://i.imgur.com/oElCs08.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//BlackBreeze
+[4078068,"Polar Bear","https://i.imgur.com/FcfUDFj.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Ztlabraptor211
+[3565766,"Southern Cassowary","https://i.imgur.com/QePt5B8.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//PrimevalBrony
+[3553036,"Saola","https://i.imgur.com/h0MFYbO.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Panthera Onca
+[6148649,"Jaguar","https://i.imgur.com/tEhdtj9.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//dycki1231
+[3812531,"Ocean Sunfish","https://i.imgur.com/0dwx3SP.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Jenna
+[3524135,"Reticulated Giraffe","https://i.imgur.com/HMBfhXV.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//xBirdy
+[4141321,"Orca","https://i.imgur.com/fljlqnd.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Enzi
+[3704174,"Cheetah","https://i.imgur.com/KpVV2Dw.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Anthracite
+[6072746,"Banded Garden Snail","https://i.imgur.com/MMTviVJ.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Zhadow
+[3907997,"Jaguar","https://i.imgur.com/tEhdtj9.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Tunes
+[3813924,"American Barn Owl","https://i.imgur.com/2UTXS3Y.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//cassynatorium
+[4201668,"Bhutan Takin","https://i.imgur.com/k8w3G0V.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Kevin
+[3526525,"Grandidier's Mongoose","https://i.imgur.com/jQ7NmI3.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Hmida0123
+[4072455,"Asian Elephant","https://i.imgur.com/jBFj4T1.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//godsaurus
+[4037983,"Ground Pangolin","https://i.imgur.com/w8tm6FQ.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//its.me.chantal
+[6159461,"Andalusian Horse","https://i.imgur.com/mZ947R9.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//tylermaster2
+[3800172,"African Bush Elephant","https://i.imgur.com/lhs50OM.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Hugo7
+[4032164,"Red Panda","https://i.imgur.com/UNBvGI2.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Lucy Joan
+[6160241,"Andalusian Horse","https://i.imgur.com/mZ947R9.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Bhutan
+[3859114,"Hippopotamus","https://i.imgur.com/oPf1f0u.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//jestorty
+[3761861,"Scottish Highland Cattle","https://i.imgur.com/wDoBXDC.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//SLGray
+[3526222,"Humpback Whale","https://i.imgur.com/A2JEZzV.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//ZS1972
+[4228609,"Secretary Bird","https://i.imgur.com/f4wjcti.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//mikek21
+[3601243,"Lion-Tailed Macaque","https://i.imgur.com/f7sNmHl.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Batty
+[4046979,"Big Brown Bat","https://i.imgur.com/hYeJR7D.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Felidae
+[6156691,"Grey Wolf","https://i.imgur.com/63WpNhx.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Natblida
+[3832669,"Roseate Spoonbill","https://i.imgur.com/FbLApTx.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Indigo and Jinn
+[6094540,"Suffolk Sheep","https://i.imgur.com/LixHWDe.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//prince_medion
+[3597927,"Long-Tailed Chinchilla","https://i.imgur.com/vzFhKti.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Ecnd
+[3656713,"Giant Armadillo","https://i.imgur.com/K6ZMbhB.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Paleop
+[4031981,"Edmontosaurus regalis","https://i.imgur.com/C90KDBd.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Anton
+[3526393,"Long-Wattled Umbrellabird","https://i.imgur.com/doWtE9W.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+
+//Zoo Tycooner FR
+[3958058,"Rufous-Tailed Scrub Robin","https://i.imgur.com/3joeY2j.png","Merry Christmas and a happy New Year!","January 1, 2017"],
+]
+
+}
+
+//]]>
